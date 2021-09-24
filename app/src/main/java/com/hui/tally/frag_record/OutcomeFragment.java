bf0879d56146b96695b7ba209d7320c523fe47bf
Spr-Aut <ff1493706520@163.com>
2021-09-24 23:50:53 +0800

    v0.5 软键盘成功（重写了OutcomFragment.java）

--- app/src/main/java/com/hui/tally/frag_record/OutcomeFragment.java
+++ app/src/main/java/com/hui/tally/frag_record/OutcomeFragment.java
@@ -15,68 +15,21 @@ import androidx.fragment.app.Fragment;
 import com.hui.tally.R;
 import com.hui.tally.utils.KeyBoardUtils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OutcomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class OutcomeFragment extends Fragment {
-
+public class OutcomeFragment extends Fragment{
     KeyboardView keyboardView;
     EditText moneyEt;
     ImageView typeIv;
     TextView typeTv,remarkTv,timeTv;
     GridView typeGv;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OutcomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OutcomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OutcomeFragment newInstance(String param1, String param2) {
-        OutcomeFragment fragment = new OutcomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_outcome, container, false);
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.fragment_outcome,container,false);
+        initView(view);
         return view;
     }
 
-    public void initView(View view){
+    private void initView(View view) {
         keyboardView=view.findViewById(R.id.frag_record_keyboard);
         moneyEt=view.findViewById(R.id.frag_record_et_money);
         typeIv=view.findViewById(R.id.frag_record_iv);
@@ -85,17 +38,16 @@ public class OutcomeFragment extends Fragment {
         remarkTv=view.findViewById(R.id.frag_record_tv_remark);
         timeTv=view.findViewById(R.id.frag_record_tv_time);
         //显示自定义软键盘
-        KeyBoardUtils boardUtils=new  KeyBoardUtils(keyboardView,moneyEt);
+        KeyBoardUtils boardUtils=new KeyBoardUtils(keyboardView,moneyEt);
         boardUtils.showKeyboard();
-        //设置接口，监听确定按钮被点击了
+        //设置接口，监听确定按钮被点击
         boardUtils.setOnEnsureListener(new KeyBoardUtils.OnEnsureListener() {
             @Override
             public void onEnsure() {
                 //点击了确定按钮
-                //获取记录的信息，保存在数据库中
+                //获取记录的信息
                 //返回上一级页面
             }
         });
     }
-
 }
